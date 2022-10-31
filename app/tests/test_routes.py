5eee65316f2d58a5f0623f96e011bfd90940a7c1
kellikuramoto <kkuramot@andrew.cmu.edu>
2022-10-30 21:35:43 -0400

    Revert "Added swagger documentation"
    This reverts commit 4597513a7c0c8cf122e30174470a21653eba8881.

--- app/tests/test_routes.py
+++ app/tests/test_routes.py
@@ -2,6 +2,7 @@ from flask import Flask
 
 from app.handlers.routes import configure_routes
 
+
 def test_base_route():
     app = Flask(__name__)
     configure_routes(app)
@@ -12,25 +13,3 @@ def test_base_route():
 
     assert response.status_code == 200
     assert response.get_data() == b'try the predict route it is great!'
-
-def test_correctAcceptance():
-    app = Flask(__name__)
-    configure_routes(app)
-    client = app.test_client()
-    url = '/predit&studyTime?=4&failures?=1&internet=true'
-
-    response = client.get(url)
-
-    assert response.status_code == 200
-    assert response.get_data() == {true}
-
-def test_correctDenial():
-    app = Flask(__name__)
-    configure_routes(app)
-    client = app.test_client()
-    url = '/predit&studyTime?=0&failures?=8&internet=false'
-
-    response = client.get(url)
-
-    assert response.status_code == 200
-    assert response.get_data() == {false}
