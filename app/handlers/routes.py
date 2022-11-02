194423ec1a42055880e7861a330192b902e0cd05
kellikuramoto <kkuramot@andrew.cmu.edu>
2022-11-01 20:43:03 -0400

    Changed features to G1, G2, failures

--- app/handlers/routes.py
+++ app/handlers/routes.py
@@ -16,45 +16,45 @@ def configure_routes(app):
         return "try the predict route it is great!"
 
 
-    @app.route('/predict_on_health')
-    def predict_health():
-        # use entries from the query string here but could also use json
-        # implictly, convert to int 0, sp raise 400
-        age = int(request.args.get('age'))
-        absences = int(request.args.get('absences'))
-        health = int(request.args.get('health'))
-        # check the range for age, absences, health here
-        if (not (age >= 15 and age <= 22)):
-            return "Invalid Request Field", 400
-        if (not (absences >= 0 and absences <= 93)):
-            return "Invalid Request Field", 400
-        if (not (health >= 1 and health <= 5)):
-            return "Invalid Request Field", 400
-        query_df = pd.DataFrame({
-            'age': pd.Series(age),
-            'health': pd.Series(health),
-            'absences': pd.Series(absences)
-        })
-        # don't use pd.get_dummies
-        prediction = clf.predict(query_df)
-        return jsonify(np.ndarray.item(prediction))
+    # @app.route('/predict_on_health')
+    # def predict_health():
+    #     # use entries from the query string here but could also use json
+    #     # implictly, convert to int 0, sp raise 400
+    #     age = int(request.args.get('age'))
+    #     absences = int(request.args.get('absences'))
+    #     health = int(request.args.get('health'))
+    #     # check the range for age, absences, health here
+    #     if (not (age >= 15 and age <= 22)):
+    #         return "Invalid Request Field", 400
+    #     if (not (absences >= 0 and absences <= 93)):
+    #         return "Invalid Request Field", 400
+    #     if (not (health >= 1 and health <= 5)):
+    #         return "Invalid Request Field", 400
+    #     query_df = pd.DataFrame({
+    #         'age': pd.Series(age),
+    #         'health': pd.Series(health),
+    #         'absences': pd.Series(absences)
+    #     })
+    #     # don't use pd.get_dummies
+    #     prediction = clf.predict(query_df)
+    #     return jsonify(np.ndarray.item(prediction))
 
     @app.route('/predict_on_study')
     def predict_study():
         #use entries from the query string here but could also use json
-        studytime = int(request.args.get('studytime'))
-        absences = int(request.args.get('absences'))
+        G1 = int(request.args.get('G1'))
+        G2 = int(request.args.get('G2'))
         failures = int(request.args.get('failures'))
-        if (not (studytime >= 1 and studytime <= 4)):
+        if (not (G1 >= 0 and G1 <= 20)):
             return "Invalid Request Field", 400
-        if (not (absences >= 0 and absences <= 93)):
+        if (not (G2 >= 0 and G2 <= 20)):
             return "Invalid Request Field", 400
         if (not (failures >= 1 and failures <= 4)):
             return "Invalid Request Field", 400
         query_df = pd.DataFrame({
-            'studytime': pd.Series(studytime),
-            'failures': pd.Series(failures),
-            'absences': pd.Series(absences)
+            'G1': pd.Series(G1),
+            'G2': pd.Series(G2),
+            'failures': pd.Series(failures)
         })
         prediction = clf.predict(query_df)
         return jsonify(np.ndarray.item(prediction))
--- app/tests/test_routes.py
+++ app/tests/test_routes.py
@@ -15,26 +15,26 @@ def test_base_route():
     assert response.get_data() == b'try the predict route it is great!'
 
 
-def test_predict_health_route():
-    app = Flask(__name__)
-    configure_routes(app)
-    client = app.test_client()
-    url = "/predict_on_health"
-    query_1 = {"age":"18","absences":"9","health":"5"}
-    query_2 = {"bad_age_name":"18","absences":"9","health":"5"}
-    query_3 = {"age":"189999999","absences":"9","health":"5"}
+# def test_predict_health_route():
+#     app = Flask(__name__)
+#     configure_routes(app)
+#     client = app.test_client()
+#     url = "/predict_on_health"
+#     query_1 = {"age":"18","absences":"9","health":"5"}
+#     query_2 = {"bad_age_name":"18","absences":"9","health":"5"}
+#     query_3 = {"age":"189999999","absences":"9","health":"5"}
 
-    response = client.get(url, query_string = query_1)
-    assert response.status_code == 200
+#     response = client.get(url, query_string = query_1)
+#     assert response.status_code == 200
 
-    response = client.get(url, query_string = "")
-    assert response.status_code == 500
+#     response = client.get(url, query_string = "")
+#     assert response.status_code == 500
 
-    response = client.get(url, query_string = query_2)
-    assert response.status_code == 500
+#     response = client.get(url, query_string = query_2)
+#     assert response.status_code == 500
 
-    response = client.get(url, query_string = query_3)
-    assert response.status_code == 400
+#     response = client.get(url, query_string = query_3)
+#     assert response.status_code == 400
 
 
 
@@ -43,18 +43,18 @@ def test_predict_study_route():
     configure_routes(app)
     client = app.test_client()
     url = "/predict_on_study"
-    query_1 = {"age":"18","absences":"9","health":"5"}
-    query_2 = {"studytime":"3","absences":"9","failures":"2"}
-    query_3 = {"studytime":"3","absences":"9","failures":"5"}
+    query_1 = {"G1":"18","G2":"9","failures":"4"}
+    query_2 = {"F1":"3","G2":"17","failures":"2"}
+    query_3 = {"G1":"3333","G2":"9","failures":"5"}
 
     response = client.get(url, query_string = query_1)
-    assert response.status_code == 500
+    assert response.status_code == 200
 
     response = client.get(url, query_string = "")
     assert response.status_code == 500
 
     response = client.get(url, query_string = query_2)
-    assert response.status_code == 200
+    assert response.status_code == 500
 
     response = client.get(url, query_string = query_3)
     assert response.status_code == 400
