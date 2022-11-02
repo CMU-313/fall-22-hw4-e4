3862c3bac82be064641076446de05d1261b4d35e
kellikuramoto <kkuramot@andrew.cmu.edu>
2022-11-01 20:55:22 -0400

    Removed comments

--- app/handlers/routes.py
+++ app/handlers/routes.py
@@ -15,30 +15,6 @@ def configure_routes(app):
     def hello():
         return "try the predict route it is great!"
 
-
-    # @app.route('/predict_on_health')
-    # def predict_health():
-    #     # use entries from the query string here but could also use json
-    #     # implictly, convert to int 0, sp raise 400
-    #     age = int(request.args.get('age'))
-    #     absences = int(request.args.get('absences'))
-    #     health = int(request.args.get('health'))
-    #     # check the range for age, absences, health here
-    #     if (not (age >= 15 and age <= 22)):
-    #         return "Invalid Request Field", 400
-    #     if (not (absences >= 0 and absences <= 93)):
-    #         return "Invalid Request Field", 400
-    #     if (not (health >= 1 and health <= 5)):
-    #         return "Invalid Request Field", 400
-    #     query_df = pd.DataFrame({
-    #         'age': pd.Series(age),
-    #         'health': pd.Series(health),
-    #         'absences': pd.Series(absences)
-    #     })
-    #     # don't use pd.get_dummies
-    #     prediction = clf.predict(query_df)
-    #     return jsonify(np.ndarray.item(prediction))
-
     @app.route('/predict_on_study')
     def predict_study():
         #use entries from the query string here but could also use json
--- app/tests/test_routes.py
+++ app/tests/test_routes.py
@@ -14,30 +14,6 @@ def test_base_route():
     assert response.status_code == 200
     assert response.get_data() == b'try the predict route it is great!'
 
-
-# def test_predict_health_route():
-#     app = Flask(__name__)
-#     configure_routes(app)
-#     client = app.test_client()
-#     url = "/predict_on_health"
-#     query_1 = {"age":"18","absences":"9","health":"5"}
-#     query_2 = {"bad_age_name":"18","absences":"9","health":"5"}
-#     query_3 = {"age":"189999999","absences":"9","health":"5"}
-
-#     response = client.get(url, query_string = query_1)
-#     assert response.status_code == 200
-
-#     response = client.get(url, query_string = "")
-#     assert response.status_code == 500
-
-#     response = client.get(url, query_string = query_2)
-#     assert response.status_code == 500
-
-#     response = client.get(url, query_string = query_3)
-#     assert response.status_code == 400
-
-
-
 def test_predict_study_route():
     app = Flask(__name__)
     configure_routes(app)
